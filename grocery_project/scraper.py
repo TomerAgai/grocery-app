8ab4deb2cb6128a29305535c46984e58c2eb6d24
TomerAgai <tomer.agai@post.runi.ac.il>
2023-06-01 11:32:45 +0300

    scraper sums prices and has notfound list

--- grocery_project/scraper.py
+++ grocery_project/scraper.py
@@ -7,18 +7,10 @@ async def fetch(session, url):
     async with session.get(url) as response:
         return await response.text()
 
-# async def fetch(session, url, filename):
-#     async with session.get(url) as response:
-#         html_content = await response.text()
-#         with open(filename, 'w', encoding='utf-8') as f:
-#             f.write(html_content)
-#         return html_content
-
 
 async def scrape_yochananof(session, product_name):
     try:
         url = f'https://yochananof.co.il/s59/catalogsearch/result/?q={product_name}'
-        # html_content = await fetch(session, url, 'yochananof_search.html')
         html_content = await fetch(session, url)
         html = pq(html_content)
         price_element = html('.price:first')
@@ -42,13 +34,32 @@ async def scrape_shufersal(session, product_name):
         return None
 
 
-async def main():
+async def main(product_list):
+    not_found_list_yochananof = []
+    not_found_list_shufersal = []
+    yochananof_total_price = 0.0
+    shufersal_total_price = 0.0
+
     async with aiohttp.ClientSession() as session:
-        product_list = ['חלב', 'ביצים']  # Your list of products in Hebrew
         for product in product_list:
             yochananof_price = await scrape_yochananof(session, product)
             shufersal_price = await scrape_shufersal(session, product)
-            print(
-                f"For {product}, Yochananof price: {yochananof_price},  Shufersal price: {shufersal_price}")
 
-asyncio.run(main())
+            if yochananof_price is None:
+                not_found_list_yochananof.append(product)
+            else:
+                yochananof_total_price += yochananof_price
+
+            if shufersal_price is None:
+                not_found_list_shufersal.append(product)
+            else:
+                shufersal_total_price += shufersal_price
+
+    print(f'Yochananof total price: {yochananof_total_price}')
+    print(f'Shufersal total price: {shufersal_total_price}')
+    print(f'Not found in Yochananof: {not_found_list_yochananof}')
+    print(f'Not found in Shufersal: {not_found_list_shufersal}')
+
+
+product_list = ['חלב', 'ביצים']  # Your list of products in Hebrew
+asyncio.run(main(product_list))
