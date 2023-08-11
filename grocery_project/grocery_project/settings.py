56b359f3664077aaa84e79b9894d05d0eab694d3
TomerAgai <tomer.agai@post.runi.ac.il>
2023-08-11 19:11:31 +0300

    replaced mysqlclient with PyMySQL

--- grocery_project/grocery_project/settings.py
+++ grocery_project/grocery_project/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+try:
+    import pymysql
+    pymysql.install_as_MySQLdb()
+except ImportError:
+    pass
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
